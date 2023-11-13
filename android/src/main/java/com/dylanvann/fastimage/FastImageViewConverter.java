7ef393fa48822099c17203ef8c6ce6480b782a0c
Aleksandr Gorbunov <lexgorbunov@gmail.com>
2023-11-13 16:15:25 +0300

    Android cache's tiers

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewConverter.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewConverter.java
@@ -26,128 +26,136 @@ import java.util.Map;
 import javax.annotation.Nullable;
 
 class FastImageViewConverter {
-    private static final Drawable TRANSPARENT_DRAWABLE = new ColorDrawable(Color.TRANSPARENT);
+  private static final Drawable TRANSPARENT_DRAWABLE = new ColorDrawable(Color.TRANSPARENT);
 
-    private static final Map<String, FastImageCacheControl> FAST_IMAGE_CACHE_CONTROL_MAP =
-            new HashMap<String, FastImageCacheControl>() {{
-                put("immutable", FastImageCacheControl.IMMUTABLE);
-                put("web", FastImageCacheControl.WEB);
-                put("cacheOnly", FastImageCacheControl.CACHE_ONLY);
-            }};
+  private static final Map<String, FastImageCacheControl> FAST_IMAGE_CACHE_CONTROL_MAP =
+    new HashMap<String, FastImageCacheControl>() {{
+      put("immutable", FastImageCacheControl.IMMUTABLE);
+      put("web", FastImageCacheControl.WEB);
+      put("cacheOnly", FastImageCacheControl.CACHE_ONLY);
+    }};
 
-    private static final Map<String, Priority> FAST_IMAGE_PRIORITY_MAP =
-            new HashMap<String, Priority>() {{
-                put("low", Priority.LOW);
-                put("normal", Priority.NORMAL);
-                put("high", Priority.HIGH);
-            }};
+  private static final Map<String, Priority> FAST_IMAGE_PRIORITY_MAP =
+    new HashMap<String, Priority>() {{
+      put("low", Priority.LOW);
+      put("normal", Priority.NORMAL);
+      put("high", Priority.HIGH);
+    }};
 
-    private static final Map<String, ImageView.ScaleType> FAST_IMAGE_RESIZE_MODE_MAP =
-            new HashMap<String, ImageView.ScaleType>() {{
-                put("contain", ScaleType.FIT_CENTER);
-                put("cover", ScaleType.CENTER_CROP);
-                put("stretch", ScaleType.FIT_XY);
-                put("center", ScaleType.CENTER_INSIDE);
-            }};
+  private static final Map<String, ImageView.ScaleType> FAST_IMAGE_RESIZE_MODE_MAP =
+    new HashMap<String, ImageView.ScaleType>() {{
+      put("contain", ScaleType.FIT_CENTER);
+      put("cover", ScaleType.CENTER_CROP);
+      put("stretch", ScaleType.FIT_XY);
+      put("center", ScaleType.CENTER_INSIDE);
+    }};
 
-    // Resolve the source uri to a file path that android understands.
-    static @Nullable
-    FastImageSource getImageSource(Context context, @Nullable ReadableMap source) {
-        return source == null
-                ? null
-                : new FastImageSource(context, source.getString("uri"), getHeaders(source));
+  // Resolve the source uri to a file path that android understands.
+  static @Nullable
+  FastImageSource getImageSource(Context context, @Nullable ReadableMap source) {
+    return source == null
+      ? null
+      : new FastImageSource(context, getSourceWithCacheParams(source), getHeaders(source));
+  }
+
+  static String getSourceWithCacheParams(@Nullable ReadableMap source) {
+    if (source == null) return "";
+    String tierName = source.getString("cacheTier");
+    String baseSource = source.getString("uri");
+    if (tierName == null || tierName.equals("")) return baseSource;
+    return baseSource + "#" + ExtraDiskCacheAdapter.TIER_PREFIX + "-" + tierName;
+  }
+
+  static Headers getHeaders(ReadableMap source) {
+    Headers headers = Headers.DEFAULT;
+
+    if (source.hasKey("headers")) {
+      ReadableMap headersMap = source.getMap("headers");
+      ReadableMapKeySetIterator iterator = headersMap.keySetIterator();
+      LazyHeaders.Builder builder = new LazyHeaders.Builder();
+
+      while (iterator.hasNextKey()) {
+        String header = iterator.nextKey();
+        String value = headersMap.getString(header);
+
+        builder.addHeader(header, value);
+      }
+
+      headers = builder.build();
     }
 
-    static Headers getHeaders(ReadableMap source) {
-        Headers headers = Headers.DEFAULT;
+    return headers;
+  }
 
-        if (source.hasKey("headers")) {
-            ReadableMap headersMap = source.getMap("headers");
-            ReadableMapKeySetIterator iterator = headersMap.keySetIterator();
-            LazyHeaders.Builder builder = new LazyHeaders.Builder();
-
-            while (iterator.hasNextKey()) {
-                String header = iterator.nextKey();
-                String value = headersMap.getString(header);
-
-                builder.addHeader(header, value);
-            }
-
-            headers = builder.build();
-        }
-
-        return headers;
+  static RequestOptions getOptions(Context context, FastImageSource imageSource, ReadableMap source) {
+    // Get priority.
+    final Priority priority = FastImageViewConverter.getPriority(source);
+    // Get cache control method.
+    final FastImageCacheControl cacheControl = FastImageViewConverter.getCacheControl(source);
+    DiskCacheStrategy diskCacheStrategy = DiskCacheStrategy.AUTOMATIC;
+    boolean onlyFromCache = false;
+    boolean skipMemoryCache = false;
+    switch (cacheControl) {
+      case WEB:
+        // If using none then OkHttp integration should be used for caching.
+        diskCacheStrategy = DiskCacheStrategy.NONE;
+        skipMemoryCache = true;
+        break;
+      case CACHE_ONLY:
+        onlyFromCache = true;
+        break;
+      case IMMUTABLE:
+        // Use defaults.
+        break;
     }
 
-    static RequestOptions getOptions(Context context, FastImageSource imageSource, ReadableMap source) {
-        // Get priority.
-        final Priority priority = FastImageViewConverter.getPriority(source);
-        // Get cache control method.
-        final FastImageCacheControl cacheControl = FastImageViewConverter.getCacheControl(source);
-        DiskCacheStrategy diskCacheStrategy = DiskCacheStrategy.AUTOMATIC;
-        boolean onlyFromCache = false;
-        boolean skipMemoryCache = false;
-        switch (cacheControl) {
-            case WEB:
-                // If using none then OkHttp integration should be used for caching.
-                diskCacheStrategy = DiskCacheStrategy.NONE;
-                skipMemoryCache = true;
-                break;
-            case CACHE_ONLY:
-                onlyFromCache = true;
-                break;
-            case IMMUTABLE:
-                // Use defaults.
-                break;
-        }
+    RequestOptions options = new RequestOptions()
+      .diskCacheStrategy(diskCacheStrategy)
+      .onlyRetrieveFromCache(onlyFromCache)
+      .skipMemoryCache(skipMemoryCache)
+      .priority(priority)
+      .placeholder(TRANSPARENT_DRAWABLE);
 
-        RequestOptions options = new RequestOptions()
-                .diskCacheStrategy(diskCacheStrategy)
-                .onlyRetrieveFromCache(onlyFromCache)
-                .skipMemoryCache(skipMemoryCache)
-                .priority(priority)
-                .placeholder(TRANSPARENT_DRAWABLE);
-
-        if (imageSource.isResource()) {
-            // Every local resource (drawable) in Android has its own unique numeric id, which are
-            // generated at build time. Although these ids are unique, they are not guaranteed unique
-            // across builds. The underlying glide implementation caches these resources. To make
-            // sure the cache does not return the wrong image, we should clear the cache when the
-            // application version changes. Adding a cache signature for only these local resources
-            // solves this issue: https://github.com/DylanVann/react-native-fast-image/issues/402
-            options = options.apply(signatureOf(ApplicationVersionSignature.obtain(context)));
-        }
-
-        return options;
+    if (imageSource.isResource()) {
+      // Every local resource (drawable) in Android has its own unique numeric id, which are
+      // generated at build time. Although these ids are unique, they are not guaranteed unique
+      // across builds. The underlying glide implementation caches these resources. To make
+      // sure the cache does not return the wrong image, we should clear the cache when the
+      // application version changes. Adding a cache signature for only these local resources
+      // solves this issue: https://github.com/DylanVann/react-native-fast-image/issues/402
+      options = options.apply(signatureOf(ApplicationVersionSignature.obtain(context)));
     }
 
-    private static FastImageCacheControl getCacheControl(ReadableMap source) {
-        return getValueFromSource("cache", "immutable", FAST_IMAGE_CACHE_CONTROL_MAP, source);
-    }
+    return options;
+  }
 
-    private static Priority getPriority(ReadableMap source) {
-        return getValueFromSource("priority", "normal", FAST_IMAGE_PRIORITY_MAP, source);
-    }
+  private static FastImageCacheControl getCacheControl(ReadableMap source) {
+    return getValueFromSource("cache", "immutable", FAST_IMAGE_CACHE_CONTROL_MAP, source);
+  }
 
-    static ScaleType getScaleType(String propValue) {
-        return getValue("resizeMode", "cover", FAST_IMAGE_RESIZE_MODE_MAP, propValue);
-    }
+  private static Priority getPriority(ReadableMap source) {
+    return getValueFromSource("priority", "normal", FAST_IMAGE_PRIORITY_MAP, source);
+  }
 
-    private static <T> T getValue(String propName, String defaultPropValue, Map<String, T> map, String propValue) {
-        if (propValue == null) propValue = defaultPropValue;
-        T value = map.get(propValue);
-        if (value == null)
-            throw new JSApplicationIllegalArgumentException("FastImage, invalid " + propName + " : " + propValue);
-        return value;
-    }
+  static ScaleType getScaleType(String propValue) {
+    return getValue("resizeMode", "cover", FAST_IMAGE_RESIZE_MODE_MAP, propValue);
+  }
 
-    private static <T> T getValueFromSource(String propName, String defaultProp, Map<String, T> map, ReadableMap source) {
-        String propValue;
-        try {
-            propValue = source != null ? source.getString(propName) : null;
-        } catch (NoSuchKeyException e) {
-            propValue = null;
-        }
-        return getValue(propName, defaultProp, map, propValue);
+  private static <T> T getValue(String propName, String defaultPropValue, Map<String, T> map, String propValue) {
+    if (propValue == null) propValue = defaultPropValue;
+    T value = map.get(propValue);
+    if (value == null)
+      throw new JSApplicationIllegalArgumentException("FastImage, invalid " + propName + " : " + propValue);
+    return value;
+  }
+
+  private static <T> T getValueFromSource(String propName, String defaultProp, Map<String, T> map, ReadableMap source) {
+    String propValue;
+    try {
+      propValue = source != null ? source.getString(propName) : null;
+    } catch (NoSuchKeyException e) {
+      propValue = null;
     }
+    return getValue(propName, defaultProp, map, propValue);
+  }
 }
